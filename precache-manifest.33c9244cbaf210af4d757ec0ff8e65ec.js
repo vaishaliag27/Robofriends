19802cd9a4268e5223d2e6db907fa4ab9f06cb7a
vaishaliag27 <27vaiagrawal@gmail.com>
2020-09-22 12:23:34 +0530

    Updates

---
+++ precache-manifest.33c9244cbaf210af4d757ec0ff8e65ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e73c11c7aa7712f2261d9bfca468eb",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "95e7cc8aac16361d9f95",
+    "url": "/Robofriends/static/css/main.7692bd2c.chunk.css"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/Robofriends/static/js/2.be3d66de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.be3d66de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e7cc8aac16361d9f95",
+    "url": "/Robofriends/static/js/main.cb4d9109.chunk.js"
+  },
+  {
+    "revision": "23198890a48c5ff10f0c",
+    "url": "/Robofriends/static/js/runtime-main.5c7fab83.js"
+  }
+]);
